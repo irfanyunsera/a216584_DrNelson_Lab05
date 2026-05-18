b6dd757cb55706486d11cf8b2d7db461786aeaa5
irfanyunsera <162158365+irfanyunsera@users.noreply.github.com>
2026-05-18 15:52:33 +0800

    Update CupcakeApplication.kt

--- app/src/main/java/com/example/cupcake/CupcakeApplication.kt
+++ app/src/main/java/com/example/cupcake/CupcakeApplication.kt
@@ -5,7 +5,7 @@ import com.example.cupcake.data.CupcakeDatabase
 import com.example.cupcake.data.OrderRepository
 
 class CupcakeApplication : Application() {
-    // Membina dependensi secara malas (lazy)
+    
     val database by lazy { CupcakeDatabase.getDatabase(this) }
     val repository by lazy { OrderRepository(database.orderDao()) }
 }
